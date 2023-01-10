fdac46c89a3b4119981d98fdfbf2d85e1b62e904
GoldLion1011 <yogirose79@gmail.com>
2023-01-10 20:15:58 +0000

    task 2 redo re-dodo

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,40 +1,43 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
       throw new TypeError('Name must be a string');
     }
-    this._name = value;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    if (typeof value !== 'number') {
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
       throw new TypeError('Length must be a number');
     }
-    this._length = value;
   }
 
   get students() {
     return this._students;
   }
 
-  set students(value) {
-    if (typeof value !== 'object' || value.every((item) => (typeof item !== 'string'))) {
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      this._students = newStudents;
+    } else {
       throw new TypeError('Students must be an array of numbers');
     }
-    this._students = value;
   }
 }
