d5ad55acc05e2b5f83aa7f117b333aafd47cf6e2
GoldLion1011 <yogirose79@gmail.com>
2023-01-13 21:03:02 +0000

    task 0 eslint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -6,9 +6,9 @@ export default function getListStudents() {
       location,
     };
   }
-  const student1 = Student(1, 'Guillaume','San Francisco');
-  const student2 = Student(2,'James','Columbia');
-  const student5 = Student(5, 'Serena','San Francisco');
+  const student1 = Student(1, 'Guillaume', 'San Francisco');
+  const student2 = Student(2, 'James', 'Columbia');
+  const student5 = Student(5, 'Serena', 'San Francisco');
 
   const studentList = [];
   studentList.push(student1, student2, student5);
