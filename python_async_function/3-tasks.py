2509f058cb95c2f2db0052f5f29860c2fc1f27a5
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-09 14:37:00 -0500

    task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""Measure the runtime"""
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Create a task that waits for a random delay
+        between 0 and max_delay seconds."""
+    return asyncio.create_task(wait_random(max_delay))
