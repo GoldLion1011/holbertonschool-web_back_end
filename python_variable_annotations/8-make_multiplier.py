392343e6a338b7684d8b768363d2f249160a0251
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-09 11:35:40 -0500

    task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""Complex types - list of floats"""
+from typing import List, Union
+
+
+def make_multiplier(multiplier: float) -> callable:
+    """Returns a function that multiplies a float by multiplier
+    Args:
+        multiplier (float): multiplier
+    Returns:
+        callable: function that multiplies a float by multiplier
+    """
+    def multiply_by_multiplier(x: float) -> float:
+        """Multiplies a float by multiplier
+        Args:
+            x (float): number to multiply
+        Returns:
+            float: x multiplied by multiplier
+        """
+        return x * multiplier
+    return multiply_by_multiplier
