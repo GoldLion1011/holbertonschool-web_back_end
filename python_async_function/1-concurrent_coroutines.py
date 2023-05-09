9774c548d8cb64d80652dcf30948aa83252ab443
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-09 14:13:13 -0500

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Concurrent coroutines"""
+wait_random = __import__('0-basic_async_syntax').wait_random
+import asyncio
+import random
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """Spawn wait_random n times with the specified max_delay."""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = []
+    for coro in asyncio.as_completed(tasks):
+        delay = await coro
+        delays.append(delay)
+    return sorted(delays)
