f41ca49868188a5bf5c99efbf6f68d0bf982ca36
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-09 11:50:40 -0500

    task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""I'm not sure what I'm looking at, much less doing here"""
+from typing import Sequence, Tuple, List, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Holy TaMoly, Batman! What is this?"""
+    return [(i, len(i)) for i in lst]
