3562ff06673283be0b8fd3cb0132cc927d3cb1e9
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-09 10:54:28 -0500

    task 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""This module contains the function floor which takes a float n as argument
+    and returns the floor of the float."""
+import math
+
+
+def floor(n: float) -> int:
+    """This function takes a float n as argument and returns the floor of the
+        float."""
+    return math.floor(n)
