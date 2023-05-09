903868ddd57f10ee9406513cdffadf9d669df448
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-09 11:24:41 -0500

    task 5, 6

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""Complex types - list of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sum of list of floats
+    Args:
+        input_list (List[float]): list of floats
+    Returns:
+        float: sum of list of floats
+    """
+    return sum(input_list)
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Complex types - mixed list"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Sum of list of floats
+    Args:
+        mxd_lst (List[Union[int, float]]): list of floats
+    Returns:
+        float: sum of list of floats
+    """
+    return sum(mxd_lst)
