67d79e16eaa0aaa2db02f47797d663430910ba96
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-09 14:42:38 -0500

    task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Measure the runtime"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn n tasks that wait for a random delay
+        between 0 and max_delay seconds."""
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
