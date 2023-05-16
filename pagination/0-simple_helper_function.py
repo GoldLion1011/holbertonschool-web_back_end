896ec9077c2b4d256e17eeb4f12b989623eb6139
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-16 10:41:06 -0500

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+""" Simple helper function """
+
+from typing import Tuple
+
+
+def index_range(page, page_size):
+    """ Return a tuple of size two containing a start index and an end index
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
