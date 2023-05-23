de3682fa8b81e0d414dbafa54f839b2ba62f22cd
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-23 16:52:37 -0500

    task 5 corrected

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -42,17 +42,14 @@ def not_found(error) -> str:
 @app.before_request
 def before_request():
     """ Filters request to error handlers"""
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/',
+    authList = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth and auth.require_auth(request.path, authList):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
