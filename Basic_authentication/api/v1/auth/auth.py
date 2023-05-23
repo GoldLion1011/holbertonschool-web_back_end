9631f76c7c0737641ce672d9e2317b0e70d969c7
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-23 16:10:09 -0500

    task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Module of authentication views """
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ API authentication class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method that returns False - path and excluded_paths """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Method that returns None - request """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method that returns None - request """
+        return None
