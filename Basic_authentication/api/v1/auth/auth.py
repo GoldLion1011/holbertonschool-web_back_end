8df6dcad8fa23a4188815b6149269c7a28fb39f7
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-23 16:45:36 -0500

    task 5

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,6 @@ class Auth():
             return True
         if path in excluded_paths or path + '/' in excluded_paths:
             return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
