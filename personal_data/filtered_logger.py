80a1096f8e4c7f39444e4ad194b434ccbf178b40
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-23 10:05:15 -0500

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 
-import typing
+from typing import List
 import re
 import logging
 
 
-def filter_datum(fields: typing.List[str], redaction: str, message: str,
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Redacting Formatter class """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in incoming log """
+        msg = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
