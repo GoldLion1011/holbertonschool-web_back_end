801e30fe3a894509f5d76790b3b8353fd6db656c
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-23 17:17:55 -0500

    task 6

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -15,10 +15,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 auth = None
-auth_type = getenv("AUTH_TYPE")
+auth_type = getenv("AUTH_TYPE", "basic")
 if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(401)
---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module of authentication views """
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ API authentication class """
+    pass
