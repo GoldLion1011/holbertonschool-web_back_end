6ed427fcadc155e0ea3408786ca2c596ad75f093
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-24 10:26:21 -0500

    task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,3 +16,16 @@ class BasicAuth(Auth):
         if authorization_header[0:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                            base64_authorization_header: str) -> str:
+        """ Method that returns decoded value of
+            base64_authorization_header """
+        if base64_authorization_header is None or type(
+                base64_authorization_header) is not str:
+            return None
+        try:
+            return base64_authorization_header.decode('utf-8')
+        except Exception:
+            return None
+
