9d59c1d87ce82b350f12f9466045dc673af6d745
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-26 14:55:43 -0500

    task 5 again again

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -4,10 +4,9 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-from flask import abort, request
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -54,13 +53,14 @@ def before_request():
                 '/api/v1/unauthorized/',
                 '/api/v1/forbidden/'
                 '/api/v1/auth_session/login/']
+    
     if auth and auth.require_auth(request.path, authList):
-        if auth.authorization_header(request) is None \
-                and auth.session_cookie(request) is None:
+        if (auth.authorization_header(request) is None \
+                and auth.session_cookie(request) is None):
             abort(401)
-        if auth.authorization_header(request) is None \
-                and auth.session_cookie(request):
-            abort(403)
+        #if auth.authorization_header(request) is None \
+                #and auth.session_cookie(request):
+            #abort(403)
         if auth.current_user(request) is None:
             abort(403)
         request.current_user = auth.current_user(request)
