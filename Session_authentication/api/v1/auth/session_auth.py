c71c08eaf889df52c043965c0e0cc9eab22c1b5c
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-26 20:26:21 -0500

    task 6 fixed

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -26,7 +26,7 @@ class SessionAuth(Auth):
 
     def current_user(self, request=None):
         """ Method that return a User instance based on a cookie value """
-        session_cookie = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(session_cookie)
-        from models import User
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        from models.user import User
         return User.get(user_id)
