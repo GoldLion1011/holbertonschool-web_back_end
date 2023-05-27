999b0deba80fa2392595c03dc04609bf9c54ae27
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-27 11:04:22 -0500

    dunno im done

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -19,12 +19,15 @@ def login():
     password = request.form.get('password')
     if not password:
         return jsonify({"error": "password missing"}), 400
-    user = User.search({'email': email})
-    if user == []:
+    try:
+        user = User.search({'email': email})
+    except Exception:
         return jsonify({"error": "no user found for this email"}), 404
-    for u in user:
-        if not u.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     SESSION_NAME = getenv('SESSION_NAME')
