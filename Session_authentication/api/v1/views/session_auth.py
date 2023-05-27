3258d29f0ae988429dac93edf813ba703a5f727a
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-27 10:44:53 -0500

    sesh id

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -7,6 +7,16 @@ from models.user import User
 from os import getenv
 
 
+def get_user_from_session(session_id):
+    """ Retrieves the user associated with a session """
+    from api.v1.app import auth
+    user_id = auth.get_user_from_session_id(session_id)
+    if not user_id:
+        return None
+    user = User.get(user_id)
+    return user
+
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """ POST /api/v1/auth_session/login
@@ -29,14 +39,26 @@ def login():
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
-    # New code: GET /api/v1/users/me
-    me_route = request.environ['PATH_INFO'] == '/api/v1/users/me'
-    if me_route:
-        return jsonify(user.to_json())
-
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     SESSION_NAME = getenv('SESSION_NAME')
     response = jsonify(user.to_json())
     response.set_cookie(SESSION_NAME, session_id)
     return response
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def get_current_user():
+    """ GET /api/v1/users/me
+    Return:
+      - User instance JSON represented
+    """
+    session_id = request.cookies.get('SESSION_NAME')
+    if not session_id:
+        return jsonify({"error": "unauthorized"}), 401
+
+    user = get_user_from_session(session_id)
+    if not user:
+        return jsonify({"error": "unauthorized"}), 401
+
+    return jsonify(user.to_json())
