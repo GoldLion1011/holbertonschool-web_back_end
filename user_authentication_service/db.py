9750e13e143992a27cf389998ad3b0d4a5b70a07
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-28 15:39:55 -0500

    task 2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,6 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
 
 from user import Base, User
 
@@ -32,3 +35,14 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Takes in arbitrary keyword arguments and returns the first row
+            found in the users table as filtered by the method’s input
+            arguments """
+        if not kwargs:
+            raise InvalidRequestError
+        row = self._session.query(User).filter_by(**kwargs).first()
+        if row is None:
+            raise NoResultFound
+        return row
