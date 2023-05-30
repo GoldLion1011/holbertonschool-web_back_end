76f78ee1c9d4c5609710e45fa821e4296fa213be
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-30 15:21:43 -0500

    sYnTaX

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Saves a user to the database and returns a User object """
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        self._session.add(User)
         self._session.commit()
         return user
 
