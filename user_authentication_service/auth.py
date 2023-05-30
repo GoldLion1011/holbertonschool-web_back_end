4996967ed19927b2fc79e5f29f803c6aeb666246
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-30 16:13:38 -0500

    task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,7 +3,25 @@
 
 
 from bcrypt import hashpw, gensalt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a user """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
 
 def _hash_password(password: str) -> str:
     """ A salted hash of the input password, hashed with bcrypt.hashpw """
