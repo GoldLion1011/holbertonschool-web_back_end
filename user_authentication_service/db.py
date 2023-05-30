f7c8df955bc570b35d0e37c634eb802b47af08c0
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-30 15:35:41 -0500

    SUCKEYS again!

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -40,9 +40,9 @@ class DB:
         """ Takes in arbitrary keyword arguments and returns the first row
             found in the users table as filtered by the method’s input
             arguments """
-        try:
-            return self._session.query(User).filter_by(**kwargs).one()
-        except InvalidRequestError:
+        if not kwargs:
             raise InvalidRequestError
-        except NoResultFound:
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
             raise NoResultFound
+        return user
