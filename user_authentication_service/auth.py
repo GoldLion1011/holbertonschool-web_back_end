17d3ad56ee80a35c7393b0d7f0b88368207e9775
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-30 16:07:32 -0500

    task 4

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+""" Auth module """
+
+
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password: str) -> str:
+    """ A salted hash of the input password, hashed with bcrypt.hashpw """
+    return hashpw(password.encode(), gensalt())
