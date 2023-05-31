5eb8901b3304f618334bba08438139e0a0bf0ee9
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-31 09:57:53 -0500

    task 6

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+""" Basic Flask Module """
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")  # Flask app instance created, URL = /
+def welcome():
+    """ Welcome Method """
+    return jsonify({"message": "Bienvenue"})  # JSON response via GET request
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
