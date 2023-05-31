7eed64b60b0f0350acc66012e1229b5fabfdd537
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-05-31 11:18:09 -0500

    task 10

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -32,6 +32,16 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """ Create a session ID """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> str:
     """ A salted hash of the input password, hashed with bcrypt.hashpw """
