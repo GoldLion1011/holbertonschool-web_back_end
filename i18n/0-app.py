42d40f1484e4e161de8e834c92c51ffff9f250ec
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-05 10:50:36 -0500

    task 0 and moar

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -2,14 +2,15 @@
 """ Flask App """
 
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    return 'Hello world'
+    """ Index page """
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', 5000, threaded=True, debug=True)
