1860bde2ec66cf7ccf9fed33f05efc3a85707eaa
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-05 09:40:21 -0500

    task 7

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -46,6 +46,15 @@ class TestGithubOrgClient(unittest.TestCase):
                              ["Test"])
             mock_get_json.assert_called_once()
 
+    @parameterized.expand([
+        ({'license': {'key': 'my_license'}}, 'my_license', True),
+        ({'license': {'key': 'other_license'}}, 'my_license', False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ Test has_license method """
+        self.assertEqual(GithubOrgClient.has_license(repo, license_key),
+                         expected)
+
     # @classmethod
     # def setUpClass(cls):
     #     """ setUpClass method """
