c3d31961aa7149581488cfa701a79b411c62e801
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-09 14:36:44 -0500

    task 0

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python3
 """ Redis exercise file """
 
+
 import redis
 import uuid
 from typing import Union, Callable, Optional
 from functools import wraps
 
 
+class Cache():
+    """ Cache class """
 
+    def __init__(self):
+        """ Constructor """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Store data in redis """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    # def get(self, key: str, fn: Optional[Callable] = None) -> Union[str,
+    #                                                                 bytes,
+    #                                                                 int,
+    #                                                                 float]:
+    #     """ Get data from redis """
+    #     data = self._redis.get(key)
+    #     if fn:
+    #         return fn(data)
+    #     return data
+
+    # def get_str(self, key: str) -> str:
+    #     """ Get string data from redis """
+    #     return self.get(key, str)
+
+    # def get_int(self, key: str) -> int:
+    #     """ Get int data from redis """
+    #     return self.get(key, int)
