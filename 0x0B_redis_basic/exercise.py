903bd68cbedf5f0cb5b8719406cbb7d7410aac07
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-09 15:32:33 -0500

    task something

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -35,6 +35,21 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable):
+    """ Replay decorator """
+    r = redis.Redis()
+    method_name = method.__qualname__
+    count = r.get(method_name).decode("utf-8")
+    inputs = r.lrange(method_name + ":inputs", 0, -1)
+    outputs = r.lrange(method_name + ":outputs", 0, -1)
+
+    print("{} was called {} times:".format(method_name, count))
+
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(method_name, i.decode("utf-8"),
+                                     o.decode("utf-8")))
+
+
 class Cache():
     """ Cache class """
 
