a0e847e5bc8b8b8605d420f7b0e6066d3309e3ca
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-13 14:10:29 -0500

    task 2 finally

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- task 2: Create a view that shows ranks of bands in various countries
-SELECT DISTINCT origin, SUM(nb_fans) AS total_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
