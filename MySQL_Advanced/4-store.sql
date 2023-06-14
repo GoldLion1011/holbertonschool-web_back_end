6f94fb556a38d24651885b7f8db5374e7c6a6afc
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-14 10:27:59 -0500

    task 4

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,1 +1,12 @@
+-- task 4: Create a trigger that decreases the quantity of an item after inserting a new order.
+DELIMITER //
+CREATE TRIGGER update_item_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.quantity
+    WHERE item_id = NEW.item_id;
+END//
+DELIMITER ;
 
