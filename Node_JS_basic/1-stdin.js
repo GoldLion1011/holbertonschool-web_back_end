0c6c0c35b9f11e5fd718bfce99c8362e09122565
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-23 13:50:10 -0500

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+// comment here about the code you are about to write
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  if (name !== '') {
+    process.stdout.write(`Your name is: ${name}\n`);
+  } else {
+    process.stdout.write('Your name is: undefined\n');
+  }
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
