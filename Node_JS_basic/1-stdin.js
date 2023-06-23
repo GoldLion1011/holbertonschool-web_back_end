bb2006911875cc8da38d25e8afb0a795115df82f
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-23 13:55:46 -0500

    task 1 fixed

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,15 @@
-// comment here about the code you are about to write
+// 1. Write a program that takes input from the user using the process.stdin.on() method.
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
+process.stdin.once('data', (data) => {
   const name = data.toString().trim();
   if (name !== '') {
     process.stdout.write(`Your name is: ${name}\n`);
   } else {
     process.stdout.write('Your name is: undefined\n');
   }
-});
 
-process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
