0234b71826b8f739aab18d151d7d6e2afad370ed
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-26 15:35:46 -0500

    task 4 barf

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,14 +2,10 @@
 
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
-});
-
-app.listen(1245, () => {
-  console.log('Server is running and listening on port 1245');
-});
+const app = http
+  .createServer((req, res) => {
+    res.end('Hello Holberton School!');
+  })
+  .listen(1245);
 
 module.exports = app;
-
