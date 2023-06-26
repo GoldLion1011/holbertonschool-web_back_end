d286e01075767ed0a7930dda6c6343359c2e9581
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-26 14:55:45 -0500

    task 2 redo redo and im done

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,44 +2,34 @@
 
 const fs = require('fs');
 
-function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
-
-    if (lines.length === 0) {
-      throw new Error('Cannot load the database');
-    }
-
-    const students = lines.map((line) => line.split(','))
-      .filter((student) => student.length === 4 && student.every((field) => field.trim() !== ''))
-      .map((student) => ({
-        firstName: student[0].trim(),
-        lastName: student[1].trim(),
-        age: parseInt(student[2].trim()),
-        field: student[3].trim(),
-      }));
-
-    const fieldCounts = {};
-    students.forEach((student) => {
-      if (!fieldCounts[student.field]) {
-        fieldCounts[student.field] = {
-          count: 0,
-          names: [],
-        };
-      }
-      fieldCounts[student.field].count++;
-      fieldCounts[student.field].names.push(student.firstName);
-    });
-
-    console.log(`Number of students: ${students.length}`);
-    Object.entries(fieldCounts).forEach(([field, { count, names }]) => {
-      console.log(`Number of students in ${field}: ${count}. List: ${names.join(', ')}`);
-    });
-  } catch (error) {
-    console.error(error.message);
+const countStudents = (file) => {
+  if (!fs.existsSync(file)) {
+    throw new Error('Cannot load the database');
   }
-}
 
-// Usage example
-countStudents('path/to/database.csv');
+  const students = fs.readFileSync(file, 'utf8');
+  let lines = students.split('\n');
+
+  lines = lines.filter((line) => line !== '').slice(1);
+  console.log(`Number of students: ${lines.length}`);
+
+  const field = lines.map((line) => line.split(',')[3]);
+
+  const eachField = [...new Set(field)];
+
+  eachField.forEach((fieldName) => {
+    const studentsPerField = lines
+      .filter((line) => line.endsWith(fieldName))
+      .map((line) => {
+        const split = line.split(',');
+        return split[0];
+      });
+    console.log(
+      `Number of students in ${fieldName}: ${
+        studentsPerField.length
+      }. List: ${studentsPerField.join(', ')}`,
+    );
+  });
+};
+
+module.exports = countStudents;
