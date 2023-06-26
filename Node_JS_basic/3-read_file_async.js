8b6e392ce7f60ffb9b19303e092aa52fa9979379
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-06-26 15:13:12 -0500

    task 3 igiveup

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -6,25 +6,42 @@ const countStudents = async (file) => {
   let content;
   try {
     content = await fs.promises.readFile(file, 'utf8');
-  } catch (err) {
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
 
-  lines = lines.split('\n');
+  let lines = content.split('\n');
   lines = lines.filter((line) => line !== '').slice(1);
   console.log(`Number of students: ${lines.length}`);
 
   const field = lines.map((line) => line.split(',')[3]);
   const eachField = [...new Set(field)];
+
   const dict = {};
 
-  eachField.forEach((fieldName) => {
-    const studentsPerField = lines
-      .filter((line) => line.endsWith(fieldName))
-      .map((line) => {
-        const split = line.split(',');
-        return split[0];
-      });
-    dict[fieldName] = studentsPerField.length;
-  });
+  for (let i = 0; i < eachField.length; i += 1) {
+    const numStudents = field.filter(
+      (fieldName) => fieldName === eachField[i],
+    ).length;
+
+    const studentsPerField = lines.filter(
+      (line) => line.split(',')[3] === eachField[i],
+    );
+
+    const names = studentsPerField.map((line) => line.split(',')[0]);
+
+    console.log(
+      `Number of students in ${
+        eachField[i]
+      }: ${numStudents}. List: ${names.join(', ')}`,
+    );
+
+    dict[eachField[i]] = {
+      numStudents,
+      names,
+    };
+  }
+  return dict;
 };
+
+module.exports = countStudents;
