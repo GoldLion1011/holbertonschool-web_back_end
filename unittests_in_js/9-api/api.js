e974ccd751d899e179e26efa00e950dd12fb3b9a
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-07-01 15:13:22 -0500

    task 9

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,25 @@
+// an instance of express and a route for GET /.
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+const port = 7865;
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+app.get('/cart/:id', (req, res) => {
+  const cartId = req.params.id;
+
+  if (isNaN(cartId)) {
+    res.status(400).send('Invalid cart ID. Cart ID must be a number.');
+  } else {
+    res.send(`Payment methods for cart ${cartId}`);
+  }
+});
+
+module.exports = app;
